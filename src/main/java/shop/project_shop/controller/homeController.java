232102dcd03bfdc984fe6133f85ca4e7c0d73d80
SpearManCN <119284822+SpearManCN@users.noreply.github.com>
2023-11-25 13:31:10 +0900

    11.26 1st

---
+++ src/main/java/shop/project_shop/controller/homeController.java
@@ -0,0 +1,17 @@
+package shop.project_shop.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+    @RequestMapping("/")
+    public String home(Model model){
+        String data = "gogo";
+        model.addAttribute("data", data);
+        return "home";
+
+    }
+}
