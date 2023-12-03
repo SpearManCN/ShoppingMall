1f410d152ab59810768861271f94e6c6f86eaa48
SpearManCN <119284822+SpearManCN@users.noreply.github.com>
2023-12-03 20:15:59 +0900

    12.03 1st

--- src/main/java/shop/controller/ShopController.java
+++ src/main/java/shop/controller/ShopController.java
@@ -8,7 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ShopController {
     @RequestMapping("/shop")
     public String home(Model model){
-        return "shop";
+        return "shop_outer";
+
+    }
+    @RequestMapping("/shop_top")
+    public String top(Model model){
+        return "shop_top";
+
+    }
+    @RequestMapping("/shop_pants")
+    public String pants(Model model){
+        return "shop_pants";
+
+    }
+    @RequestMapping("/shop_shoes")
+    public String shoes(Model model){
+        return "shop_shoes";
 
     }
 }
