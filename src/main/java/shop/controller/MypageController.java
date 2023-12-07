d4b07b942d633dbd1db4cd58124ba9a1319f02dd
SpearManCN <119284822+SpearManCN@users.noreply.github.com>
2023-12-07 16:59:22 +0900

    12.07 2st 파일 업로드&다운로드 기능 추가

--- src/main/java/shop/controller/MypageController.java
+++ src/main/java/shop/controller/MypageController.java
@@ -2,12 +2,17 @@ package shop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 import shop.domain.BasketDTO;
 import shop.domain.JoinDTO;
 import shop.service.LoginService;
@@ -15,6 +20,8 @@ import shop.service.LoginService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -52,7 +59,23 @@ public class MypageController {
         model.addAttribute("baskets", result);
         return "mypage_basket";
     }
-
+    @RequestMapping("/pictureDown")
+    public ResponseEntity<Resource> downloadImage(HttpServletRequest request) throws
+            MalformedURLException {
+        JoinDTO joinDTO =
+                (JoinDTO) request.getSession().getAttribute("user");
+        String origin =  joinDTO.getPictureUuidName();
+        String uuid = joinDTO.getPictureUuidName();
+        UrlResource resource = new UrlResource("file:" +
+                fileDir+uuid);
+        String encodedUploadFileName = UriUtils.encode(origin,
+                StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" +
+                encodedUploadFileName + "\"";
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
+    }
 
 
 
